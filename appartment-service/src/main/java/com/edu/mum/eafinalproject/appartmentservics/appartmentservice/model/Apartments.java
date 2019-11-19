46ec1d43e6cb2aea02631b6659c9d3c6dab9f8b5
Yubraj Ghimire <yghimire1994@gmail.com>
2019-11-18 22:58:43 -0600

    Save Apartment Part Completed

--- appartment-service/src/main/java/com/edu/mum/eafinalproject/appartmentservics/appartmentservice/model/Apartments.java
+++ appartment-service/src/main/java/com/edu/mum/eafinalproject/appartmentservics/appartmentservice/model/Apartments.java
@@ -3,25 +3,141 @@ package com.edu.mum.eafinalproject.appartmentservics.appartmentservice.model;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
-@Table(value = "apartments")
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Apartments {
-    @PrimaryKey
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long apartmentId;
 
     private String apartmentTitle;
-    private String apartmentLocation;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "categoryId")
+    private Category category;
+
+    private String apartmentImages;
+    private String apartmentDescription;
     private String apartmentPhoneNumber;
+    private String apartmentWebsite;
+    private String apartmentEmail;
+    private String apartmentCostDescription;
     private double apartmentCost;
-    private String apartmentImage;
+    private String apartmentKeyword;
+    private String apartmentLocationCity;
+    private String apartmentLocationStreet;
+    private String apartmentLocationState;
+    private int apartmentLocationZipCode;
+
+    public Apartments(){}
 
 
-    public Apartments(long apartmentId, String apartmentTitle, String apartmentLocation, String apartmentPhoneNumber, double apartmentCost, String apartmentImage) {
-        this.apartmentId = apartmentId;
+    public Apartments(String apartmentTitle, String apartmentKeyword, String apartmentLocationCity, String apartmentLocationStreet, String apartmentLocationState, int apartmentLocationZipCode, String apartmentImages, String apartmentDescription, String apartmentPhoneNumber, String apartmentWebsite, String apartmentEmail, String apartmentCostDescription, double apartmentCost) {
         this.apartmentTitle = apartmentTitle;
-        this.apartmentLocation = apartmentLocation;
+        this.apartmentKeyword = apartmentKeyword;
+        this.apartmentLocationCity = apartmentLocationCity;
+        this.apartmentLocationStreet = apartmentLocationStreet;
+        this.apartmentLocationState = apartmentLocationState;
+        this.apartmentLocationZipCode = apartmentLocationZipCode;
+        this.apartmentImages = apartmentImages;
+        this.apartmentDescription = apartmentDescription;
         this.apartmentPhoneNumber = apartmentPhoneNumber;
+        this.apartmentWebsite = apartmentWebsite;
+        this.apartmentEmail = apartmentEmail;
+        this.apartmentCostDescription = apartmentCostDescription;
         this.apartmentCost = apartmentCost;
-        this.apartmentImage = apartmentImage;
+    }
+
+
+
+    public String getApartmentKeyword() {
+        return apartmentKeyword;
+    }
+
+    public void setApartmentKeyword(String apartmentKeyword) {
+        this.apartmentKeyword = apartmentKeyword;
+    }
+
+    public String getApartmentLocationCity() {
+        return apartmentLocationCity;
+    }
+
+    public void setApartmentLocationCity(String apartmentLocationCity) {
+        this.apartmentLocationCity = apartmentLocationCity;
+    }
+
+    public String getApartmentLocationStreet() {
+        return apartmentLocationStreet;
+    }
+
+    public void setApartmentLocationStreet(String apartmentLocationStreet) {
+        this.apartmentLocationStreet = apartmentLocationStreet;
+    }
+
+    public String getApartmentLocationState() {
+        return apartmentLocationState;
+    }
+
+    public void setApartmentLocationState(String apartmentLocationState) {
+        this.apartmentLocationState = apartmentLocationState;
+    }
+
+    public int getApartmentLocationZipCode() {
+        return apartmentLocationZipCode;
+    }
+
+    public void setApartmentLocationZipCode(int apartmentLocationZipCode) {
+        this.apartmentLocationZipCode = apartmentLocationZipCode;
+    }
+
+    public String getApartmentImages() {
+        return apartmentImages;
+    }
+
+    public void setApartmentImages(String apartmentImages) {
+        this.apartmentImages = apartmentImages;
+    }
+
+    public String getApartmentDescription() {
+        return apartmentDescription;
+    }
+
+    public void setApartmentDescription(String apartmentDescription) {
+        this.apartmentDescription = apartmentDescription;
+    }
+
+    public String getApartmentWebsite() {
+        return apartmentWebsite;
+    }
+
+    public void setApartmentWebsite(String apartmentWebsite) {
+        this.apartmentWebsite = apartmentWebsite;
+    }
+
+    public String getApartmentEmail() {
+        return apartmentEmail;
+    }
+
+    public void setApartmentEmail(String apartmentEmail) {
+        this.apartmentEmail = apartmentEmail;
+    }
+
+    public String getApartmentCostDescription() {
+        return apartmentCostDescription;
+    }
+
+    public void setApartmentCostDescription(String apartmentCostDescription) {
+        this.apartmentCostDescription = apartmentCostDescription;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
     public long getApartmentId() {
@@ -40,14 +156,6 @@ public class Apartments {
         this.apartmentTitle = apartmentTitle;
     }
 
-    public String getApartmentLocation() {
-        return apartmentLocation;
-    }
-
-    public void setApartmentLocation(String apartmentLocation) {
-        this.apartmentLocation = apartmentLocation;
-    }
-
     public String getApartmentPhoneNumber() {
         return apartmentPhoneNumber;
     }
@@ -64,11 +172,4 @@ public class Apartments {
         this.apartmentCost = apartmentCost;
     }
 
-    public String getApartmentImage() {
-        return apartmentImage;
-    }
-
-    public void setApartmentImage(String apartmentImage) {
-        this.apartmentImage = apartmentImage;
-    }
 }
--- appartment-service/src/main/java/com/edu/mum/eafinalproject/appartmentservics/appartmentservice/repository/ApartmentRepository.java
+++ appartment-service/src/main/java/com/edu/mum/eafinalproject/appartmentservics/appartmentservice/repository/ApartmentRepository.java
@@ -2,9 +2,10 @@ package com.edu.mum.eafinalproject.appartmentservics.appartmentservice.repositor
 
 import com.edu.mum.eafinalproject.appartmentservics.appartmentservice.model.Apartments;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ApartmentRepository extends CassandraRepository<Apartments,Long> {
+public interface ApartmentRepository extends JpaRepository<Apartments,Long> {
 
 }
